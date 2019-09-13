2cf0c13d8013ded4a36a2a5a7945fae560982e0e
Manzar H Siddique <Manzar.Hussain@maersk.com>
2019-09-13 18:09:43 +0530

    changed project structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Auxilary';
+import Aux from '../../../hoc/Auxilary/Auxilary';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
